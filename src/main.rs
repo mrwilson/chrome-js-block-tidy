b396f99cd30bf86e42c5020279449a57d0924abd
Alex Wilson <gpg@probablyfine.co.uk>
2021-05-31 18:40:22 +0100

    feat(runner): Accept `days-ago` input from user to parametrise history lookup

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,13 @@ fn main() {
                 .help("Any site with fewer than this number of visits should be removed")
                 .default_value("10"),
         )
+        .arg(
+            Arg::with_name("days-ago")
+                .required(true)
+                .long("days-ago")
+                .help("Only count visits to sites in this period")
+                .default_value("7"),
+        )
         .get_matches();
 
     let threshold = match matches.value_of("threshold").unwrap().parse::<u64>() {
@@ -22,8 +29,13 @@ fn main() {
         Err(_) => 10,
     };
 
+    let days_ago = match matches.value_of("days-ago").unwrap().parse::<u16>() {
+        Ok(n) => n,
+        Err(_) => 7,
+    };
+
     let safelist = preferences::sites_with_js_enabled();
-    let visited_sites = history::sites_visited_recently(7);
+    let visited_sites = history::sites_visited_recently(days_ago);
 
     println!(
         "{:?}",
