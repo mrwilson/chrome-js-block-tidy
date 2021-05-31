bb964497c2bd4865b422e60c09f0a4c9d90c0c4b
Alex Wilson <gpg@probablyfine.co.uk>
2021-05-31 16:14:06 +0100

    feat(preferences): Parse out urls from Preferences JSON

---
+++ src/preferences.rs
@@ -0,0 +1,78 @@
+use regex::Regex;
+use serde_json::value::Value::Bool;
+use serde_json::{Map, Value};
+
+#[derive(Debug, PartialEq)]
+pub struct SiteWithJavascriptEnabled {
+    pub url: String,
+}
+
+pub fn sites_with_js_enabled(input: &str) -> Vec<SiteWithJavascriptEnabled> {
+    let v: Value = serde_json::from_str(input).unwrap();
+
+    per_site_javascript_exceptions(&v)
+        .into_iter()
+        .filter(|(_k, v)| is_on_javascript_safelist(v))
+        .filter_map(|(key, _config)| parse_site_from_key(key))
+        .collect()
+}
+
+fn per_site_javascript_exceptions(v: &Value) -> Map<String, Value> {
+    v["profile"]["content_settings"]["exceptions"]["javascript"]
+        .as_object()
+        .unwrap()
+        .clone()
+}
+
+fn is_on_javascript_safelist(v: &Value) -> bool {
+    let safelisted = v
+        .as_object()
+        .unwrap_or(&Map::new())
+        .get("setting")
+        .unwrap_or(&Bool(false))
+        .as_u64()
+        .unwrap();
+
+    safelisted == 1
+}
+
+fn parse_site_from_key(key: String) -> Option<SiteWithJavascriptEnabled> {
+    let url = Regex::new(r"(?P<address>.*):\d+,.*")
+        .ok()?
+        .captures(&key)?
+        .name("address")?
+        .as_str()
+        .to_owned();
+
+    Some(SiteWithJavascriptEnabled { url })
+}
+
+#[cfg(test)]
+mod test {
+    use crate::preferences::{sites_with_js_enabled, SiteWithJavascriptEnabled};
+
+    #[test]
+    fn reads_js_enabled_sites() {
+        let example = r#"
+        {
+            "profile": { "content_settings": { "exceptions": { "javascript": {
+                "https://www.google.com:443,*": {
+                    "expiration": "0",
+                    "last_modified": "16188888000000",
+                    "model": 0,
+                    "setting": 1
+                }
+            }}}}
+        }"#;
+
+        let output = sites_with_js_enabled(example);
+
+        assert_eq!(output.len(), 1);
+        assert_eq!(
+            output[0],
+            SiteWithJavascriptEnabled {
+                url: "https://www.google.com".to_owned()
+            }
+        )
+    }
+}
