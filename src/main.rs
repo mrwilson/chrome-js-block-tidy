1fd3054210ff3c516a2eeda3cf764dd021a3272f
Alex Wilson <gpg@probablyfine.co.uk>
2021-05-31 18:22:40 +0100

    feat(runner): Use clap to run tool and parse arguments
    * Threshold defaults to 10 visits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::{App, Arg};
+
+mod candidates;
+mod history;
+mod preferences;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("chrome-js-block-tidy")
+        .version("0.1.0")
+        .about("A tool to manage exceptions when running Chrome without Javascript enabled")
+        .arg(
+            Arg::with_name("threshold")
+                .required(true)
+                .long("threshold")
+                .help("Any site with fewer than this number of visits should be removed")
+                .default_value("10"),
+        )
+        .get_matches();
+
+    let threshold = match matches.value_of("threshold").unwrap().parse::<u64>() {
+        Ok(n) => n,
+        Err(_) => 10,
+    };
+
+    let safelist = preferences::sites_with_js_enabled();
+    let visited_sites = history::sites_visited_recently();
+
+    println!(
+        "{:?}",
+        candidates::sites_to_remove(safelist, visited_sites, threshold)
+    );
 }
